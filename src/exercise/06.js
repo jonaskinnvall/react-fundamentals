dcc6899eb439600874a999dc4dbc3c9185ff3167
Jonas Kinnvall <jonaskinnvall@gmail.com>
2022-04-09 13:50:15 +0200

    Ex6: finish extra credit 2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,6 +5,15 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   const inputRef = React.useRef(null)
+  const [error, setError] = React.useState(null)
+
+  const handleChange = event => {
+    event.preventDefault()
+    const value = event.target.value
+    const isValid = value === value.toLowerCase()
+    console.log(isValid)
+    setError(isValid ? null : 'Username must be lower case')
+  }
 
   const handleSubmit = event => {
     event.preventDefault()
@@ -15,9 +24,16 @@ function UsernameForm({onSubmitUsername}) {
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="input">Username:</label>
-        <input id="input" type="text" ref={inputRef} />
+        <input id="input" type="text" ref={inputRef} onChange={handleChange} />
       </div>
-      <button type="submit">Submit</button>
+      {error && (
+        <div role="alert" style={{color: 'red'}}>
+          {error}
+        </div>
+      )}
+      <button disabled={error} type="submit">
+        Submit
+      </button>
     </form>
   )
 }
